09f18342e218830e088ee5cdf2237cc7402e0832
vagrant <vagrant@homestead>
2017-08-11 08:56:10 +0000

    temp update

--- components/Wechat.php
+++ components/Wechat.php
@@ -24,31 +24,64 @@ class Wechat extends \Cms\Classes\ComponentBase
      */
     public function onRun()
     {
-        $options = [
-            'debug'  => true,
-            'app_id' => Config::get('beysong.wechat::appId', 50),
-            'secret' => 'testwechat',
-            'token'  => Config::get('beysong.wechat::appSecret', 500),
-            // 'aes_key' => null, // 可选
-            'log' => [
-                'level' => 'debug',
-                'file'  => '/tmp/easywechat.log', // XXX: 绝对路径！！！！
-            ],
-            //...
-        ];
-        $wechat = app('wechat');
-        $wechat->server->setMessageHandler(function($message){
-            return "欢迎关注 overtrue！";
-        }); 
+        print_r(session('wechat.oauth_user'));
+        if(\Request::wantsJson()){
+            $options = [
+                'debug'  => true,
+                'app_id' => Config::get('beysong.wechat::appId', 50),
+                'secret' => 'testwechat',
+                'token'  => Config::get('beysong.wechat::appSecret', 500),
+                // 'aes_key' => null, // 可选
+                'log' => [
+                    'level' => 'debug',
+                    'file'  => '/tmp/easywechat.log', // XXX: 绝对路径！！！！
+                ],
+                //...
+            ];
+            $wechat = app('wechat');
+            $wechat->server->setMessageHandler(function($message){
+                switch ($message->MsgType) {
+                    case 'event':
+                        if($message->Event == 'subscribe'){
 
-        //\Log::info('return response.');
+                        }
+                        return '收到事件消息';
+                    break;
+                    case 'text':
+                    return '收到文字消息';
+                    break;
+                    case 'image':
+                    return '收到图片消息';
+                    break;
+                    case 'voice':
+                    return '收到语音消息';
+                    break;
+                    case 'video':
+                    return '收到视频消息';
+                    break;
+                    case 'location':
+                    return '收到坐标消息';
+                    break;
+                    case 'link':
+                    return '收到链接消息';
+                    break;
+                    // ... 其它消息
+                    default:
+                    return '收到其它消息';
+                    break;
+                }
+            });
 
-        return $wechat->server->serve();
+            //\Log::info('return response.');
+
+            return $wechat->server->serve();
+        }
+
         // $this->addJs('/plugins/beysong/proevent/assets/pingpp/src/pingpp-pc.js');
         // $this->addJs('/plugins/beysong/proevent/assets/js/orderview.js');
-    //	$event_id = $this->property('events');
-    //	$tickets = BeysongEvent::find($event_id)->tickets;
-    //	$this->page['tickets'] = $tickets;
+        //	$event_id = $this->property('events');
+        //	$tickets = BeysongEvent::find($event_id)->tickets;
+        //	$this->page['tickets'] = $tickets;
 
     }
 
