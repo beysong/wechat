22f6cc72700a113954ec80d3f7e88cb4b304bf01
vagrant <vagrant@homestead>
2017-09-13 11:01:49 +0000

    test code

--- components/Session.php
+++ components/Session.php
@@ -77,8 +77,8 @@ class Session extends \RainLab\User\Components\Session
             //...
         ];
 
-        // $wechat = new Application($options);
-        $wechat = app('wechat.official_account');
+        $wechat = new Application($options);
+        // $wechat = app('wechat.official_account');
         $oauth = $wechat->oauth;
         Event::listen('Overtrue\LaravelWeChat\Events\WeChatUserAuthorized', function($event)
         {
